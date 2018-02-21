c6ea3782785d676304aecd4f266a6a70b5eb568e
voffka <volodymyrotr@gmail.com>
2018-02-20 22:33:00 -0800

    Add project files.

---
+++ CpuFreqTrayIcon/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace CpuFreqTrayIcon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(120, 38);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(221, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 236);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ CpuFreqTrayIcon/Form1.cs
@@ -0,0 +1,56 @@
+﻿namespace CpuFreqTrayIcon
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Text;
+    using System.Management;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateTextIcon(uint str, uint maxsp)
+        {
+            var fontToUse = new Font("Trebuchet MS", 60, FontStyle.Regular, GraphicsUnit.Pixel);
+            var bitmapText = new Bitmap(128, 128);
+
+            var g = Graphics.FromImage(bitmapText);
+            g.Clear(Color.Blue);
+            g.FillRectangle(Brushes.DarkOrange, 0, 80, (float) str / maxsp * 128, 128);
+            g.TextRenderingHint = TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(str.ToString(), fontToUse, Brushes.White, -10, 0);
+
+            var hIcon = bitmapText.GetHicon();
+            notifyIcon1.Text = str.ToString();
+            notifyIcon1.Icon?.Dispose();
+            notifyIcon1.Icon = Icon.FromHandle(hIcon);
+            bitmapText.Dispose();
+            g.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            uint currentsp, Maxsp;
+            using (var Mo = new ManagementObject("Win32_Processor.DeviceID='CPU0'"))
+            {
+                currentsp = (uint) Mo["CurrentClockSpeed"];
+                Maxsp = (uint) Mo["MaxClockSpeed"];
+            }
+            CreateTextIcon(currentsp, Maxsp);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Visible = false;
+        }
+    }
+}
